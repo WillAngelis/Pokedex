bb9a8c2b2a5d1e68e0cb68e38becca9375588d08
William Angelis <willangelis@gmail.com>
2022-01-04 22:46:43 -0300

    feat: :sparkles: Add animation script

--- public/app.js
+++ public/app.js
@@ -1,5 +1,20 @@
+// Movement Animation to happen
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 // Pokemon Img
 const pokemon = document.querySelector('.pokemon img');
 const input = document.querySelector('.pokename');
+
+
+// Moving Animation Event
+container.addEventListener('mousemove', (e) => {
+  const xAxis = (window.innerWidth / 2 - e.pageX) / 10;
+  pokemon.style.transform = `scale(1.1) rotateX(${xAxis}deg)`;
+});
+
+container.addEventListener('mouseleave', (e) => {
+  card.style.transition = 'all 0.5s ease';
+  card.style.transform = 'rotateY(0deg) rotateX(0deg)';
+  // Popback
+  pokemon.style.transform = 'translateZ(0px)';
+});
