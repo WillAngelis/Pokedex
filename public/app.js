9e83ff3c03863fb79300616fdd4abf552de28cd3
William Angelis <willangelis@gmail.com>
2022-01-04 22:45:40 -0300

    feat: :tada: Add Javascript file

---
+++ public/app.js
@@ -0,0 +1,5 @@
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+// Pokemon Img
+const pokemon = document.querySelector('.pokemon img');
+const input = document.querySelector('.pokename');
