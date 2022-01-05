b038ff34182d74d199767258d609a90bedbbfd72
William Angelis <willangelis@gmail.com>
2022-01-04 22:52:01 -0300

    refactor: :recycle: Delete non used lines and code

--- public/app.js
+++ public/app.js
@@ -1,10 +1,9 @@
 // Movement Animation to happen
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
+
 // Pokemon Img
 const pokemon = document.querySelector('.pokemon img');
-const input = document.querySelector('.pokename');
-
 
 // Moving Animation Event
 container.addEventListener('mousemove', (e) => {
@@ -12,9 +11,9 @@ container.addEventListener('mousemove', (e) => {
   pokemon.style.transform = `scale(1.1) rotateX(${xAxis}deg)`;
 });
 
-container.addEventListener('mouseleave', (e) => {
+container.addEventListener('mouseleave', () => {
   card.style.transition = 'all 0.5s ease';
   card.style.transform = 'rotateY(0deg) rotateX(0deg)';
-  // Popback
+  // Reset Animation
   pokemon.style.transform = 'translateZ(0px)';
 });
